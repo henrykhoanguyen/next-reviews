0576ad94afb51106168471075e0404f17e6415b4
Khoa Nguyen <khoanguyen@Khoas-MacBook-Pro.local>
2023-11-24 23:14:55 -0600

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <>
+        <h1>Indie Gamer</h1>
+        <p>
+            Only the best indie game.
+        </p>
+    </>
+  );
+}
